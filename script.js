4d18a63b71faf93462786dbfecc13a05725f9f25
hehecau <161074322+hehecau@users.noreply.github.com>
2024-07-26 19:31:37 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Example of smooth scrolling (optional)
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
